35c17c018c0c73c4f8ed301d6adf2234e4e4a66d
Joe Warth <joe.warth@gmail.com>
2015-07-03 14:44:04 -0600

    Consolidating

--- MGU/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Test::Application.config.secret_token = 'e462f8143a8a15c7e7860d209b241c0f5dde20a39cdabab77dfe1c869b820135ea203190a7c2da95fe2f4cf3ced09fb02bca8ae6c1dbe2adc20815032754bbe0'
--- MGU/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Test::Application.routes.draw do
-  root :to => 'pages#index'
-
-  %w[about contact cv comic].each do |page|
-    get page, controller: 'pages', action:page
-  end
-
-end
-
